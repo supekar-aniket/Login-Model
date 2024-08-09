62cedd29e09a73aa44237c265fb77c5414096447
Aniket Supekar <supekaraniket34@gmail.com>
2024-08-09 18:31:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [menuStatus,setMenuStatus] = useState(false);
+
+  let [modelStatus,setModelStatus] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button className='menuOpenBtn' onClick={()=>{setMenuStatus(!menuStatus)}}> 
+        &#9776;        
+      </button>
+      
+      <div className={`menu ${menuStatus ? 'activeMenu' : ''}`}>
+      <button className='menuCloseBtn' onClick={()=>{setMenuStatus(!menuStatus)}}>
+        &times;
+      </button>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Course</li>
+          <li>Gallary</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <button className='loginBtn' onClick={()=>setModelStatus(!modelStatus)}>Login</button>
+
+      <div className={`modelOverlay ${modelStatus ? 'showModelOverlay' : ''}`}>
+        <div className={`login ${modelStatus ? 'showLogin' : ''}`}>
+          <div className='loginTitle'>
+            Login
+          </div>
+          <button className='modelCloseBtn' onClick={()=>{setModelStatus(!modelStatus)}}>
+            &times;
+          </button> <br/>
+          <div className='loginContainer'>
+            Name:<br/><input type='text'/> <br/>
+            Email:<br/><input type='email'/> <br/>
+            Password:<br/><input type='password'/> <br/>
+            Confirm Password:<br/><input type='password'/> <br/>
+            <button className='submitBtn'>Submit</button>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
